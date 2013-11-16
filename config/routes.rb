ab1c0423ab6447f9bf5a028dca2e0d2fcce76637
Alex <ad1444@my.bristol.ac.uk>
2013-11-16 22:00:55 +0000

    added hoem button

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Codeforgood::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'main#index'
   get '(test)/'=>'main#test'
+  get '(index)/' => 'main#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
