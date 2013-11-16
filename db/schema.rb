040ff20ed9ff1f9d3fcedeac318ed2e967e75977
claudiu <claudiu@claudiu-K72JT.(none)>
2013-11-16 21:34:49 +0000

    comapny with name

--- db/schema.rb
+++ db/schema.rb
@@ -11,21 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131116191818) do
+ActiveRecord::Schema.define(version: 20131116210603) do
 
   create_table "companies", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",                  default: false
+    t.string   "name"
   end
 
   add_index "companies", ["email"], name: "index_companies_on_email", unique: true
