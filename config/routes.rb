f5cca48def34f16fd0f958b66bbe2de595344193
ionutt93 <ioan@Ioans-MacBook-Pro.local>
2013-11-16 22:27:24 +0000

    added form, show for updates

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Codeforgood::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'main#index'
   get '(test)/'=>'main#test'
+  resources :companies
+  resources :updates
+  get 'companies/formupdate'=>'companies#formupdate'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
