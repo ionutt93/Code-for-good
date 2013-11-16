c49aaf76a092697ad8b37ac156d7563ec2a63c76
ionutt93 <ioan@Ioans-MacBook-Pro.local>
2013-11-16 16:52:04 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Codeforgood::Application.config.secret_key_base = 'dd5b8949fd110ad93c569b630b307e5273565e5eccacbf02f2ff6192a10abdb924ebf018466fffd60b59e59adc36bd7cd2c4668b42d58a5c6413bf043ac18034'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Codeforgood::Application.config.session_store :cookie_store, key: '_codeforgood_session'
